4a8f8889858fc8a67274b8a58edb64f7f384b3a9
cwi <GeekMonster842@gmail.com>
2020-10-15 23:48:57 +0000

    nbvhcgdfhjgmghn

--- main.py
+++ main.py
@@ -181,12 +181,12 @@ try:
 					brick.sety(y)
 					brick.pendown()
 					for inc in range(x[1]-x[0]):
-						brick.movex(10)
+						brick.movex(14)
 					brick.penup()
-					brick.movex(-10*(x[1]-x[0])+1)
+					brick.movex(-14*(x[1]-x[0])+1)
 					brick.penup()
 					# brick.hub.motor_A.angled(5,0.2)
-		brick.movex(10)
+		brick.movex(14)
 		if brick.color != 10 and brick.color != 0:
 			if line2:
 				brick.sety(10)
@@ -208,4 +208,4 @@ try:
 	os.system('figlet "All done!!"')
 except:
 	brick.penup()
-	os.system('cowsay  -f "$(ls /usr/share/cowsay/cows | sort -R | head -1)" "abort!!"')
+	os.system('figlet "Abort!!"')
